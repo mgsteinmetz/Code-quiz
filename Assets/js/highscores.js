dd38e80df1d28e6fdfe68841ae5f817685232d81
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-05-08 15:41:12 -0500

    adding more files to repo

---
+++ Assets/js/highscores.js
@@ -0,0 +1,8 @@
+// HIGHSCORES.html functions
+const highScoresList = document.querySelector('#highScoresList')
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+highScoresList.innerHTML = 
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("");
